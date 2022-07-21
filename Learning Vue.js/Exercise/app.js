a8c85cc47bb4db3edba3d81acb3abe06399a250d
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-21 11:54:57 +0200

    A simple exercise with vue directives and attributes

---
+++ Learning Vue.js/Exercise/app.js
@@ -0,0 +1,13 @@
+let vm = Vue.createApp({
+    data(){
+        return {
+            isPurple: false,
+            selectedColor:''
+        }
+    },
+    computed: {
+        circle_classes() {
+            return {purple: this.isPurple}
+        }
+    }
+}).mount('#app')
