31dc1f72d3066f312c24cf33833f43fb6b5ee463
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-26 00:13:42 +0200

    Understanding role of key Attribute and Maintaining state

--- Section-2/Section-2-23/app.js
+++ Section-2/Section-2-23/app.js
@@ -1,28 +1,28 @@
-// // Understanding role of key Attribute and Maintaining state
-// let vm = Vue.createApp({
-//   data() {
-//     return {
-//       people: [
-//         {
-//           name: "John",
-//           message: "Hello world!",
-//         },
-//         {
-//           name: "Rick",
-//           message: "I like pie.",
-//         },
-//         {
-//           name: "Amy",
-//           message: "Skydiving is fun!",
-//         },
-//       ],
-//     };
-//   },
-//   methods: {
-//     move() {
-//       const first = this.people.shift();
-//
-//       this.people.push(first);
-//     },
-//   },
-// }).mount("#app");
+// Understanding role of key Attribute and Maintaining state
+let vm = Vue.createApp({
+  data() {
+    return {
+      people: [
+        {
+          name: "John",
+          message: "Hello world!",
+        },
+        {
+          name: "Rick",
+          message: "I like pie.",
+        },
+        {
+          name: "Amy",
+          message: "Skydiving is fun!",
+        },
+      ],
+    };
+  },
+  methods: {
+    move() {
+      const first = this.people.shift();
+
+      this.people.push(first);
+    },
+  },
+}).mount("#app");
