86d9e171851950fd3df3c7bdf499d818e36397d5
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-26 00:12:37 +0200

    List rendering

--- Section-2/Section-2-22/app.js
+++ Section-2/Section-2-22/app.js
@@ -1,13 +1,13 @@
-// //List rendering
-// let vm = Vue.createApp({
-//   data() {
-//     return {
-//       birds: ["Pigeons", "Eagles", "Doves", "Parrots"],
-//       people: [
-//         { name: "John", age: 20 },
-//         { name: "Rick", age: 18 },
-//         { name: "Amy", age: 33 },
-//       ],
-//     };
-//   },
-// }).mount("#app");
+//List rendering
+let vm = Vue.createApp({
+  data() {
+    return {
+      birds: ["Pigeons", "Eagles", "Doves", "Parrots"],
+      people: [
+        { name: "John", age: 20 },
+        { name: "Rick", age: 18 },
+        { name: "Amy", age: 33 },
+      ],
+    };
+  },
+}).mount("#app");
