585ec8c996ffcb80551a9886f65fd16345139bdb
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-26 00:06:13 +0200

    Binding Attributes in Vue.js

---
+++ Section-2/Section-2-19/app.js
@@ -0,0 +1,15 @@
+// // Binding Classes and Styles
+// let vm = Vue.createApp({
+//   data() {
+//     return {
+//       isPurple: false,
+//       selectedColor: "",
+//       size: 150,
+//     };
+//   },
+//   computed: {
+//     circle_classes() {
+//       return { purple: this.isPurple };
+//     },
+//   },
+// }).mount("#app");
---
+++ Section-2/Section-2-21/app.js
@@ -0,0 +1,8 @@
+// // Conditional rendering and v-show directive
+// let vm = Vue.createApp({
+//   data() {
+//     return {
+//       mode: 1,
+//     };
+//   },
+// }).mount("#app");
