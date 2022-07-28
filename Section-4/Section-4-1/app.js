e35a93642ec34176d5ac8bddafce7a050fcc8443
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-28 05:29:45 +0200

    Mounting a vue instance

---
+++ Section-4/Section-4-1/app.js
@@ -0,0 +1,14 @@
+//Mounting Vue Instance
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+});
+
+vm.mount("#app");
+
+// setTimeout(() => {
+//   vm.mount("#app");
+// }, 3000);
