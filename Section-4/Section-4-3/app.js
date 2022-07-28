e90c331f574513ef798d50c8916117587a6940eb
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-29 00:45:24 +0200

    ÂUsing lifecycle hooks

--- Section-4/Section-4-3/app.js
+++ Section-4/Section-4-3/app.js
@@ -1,39 +1,39 @@
 // Using Lifecycle hooks
-// let vm = Vue.createApp({
-//   data() {
-//     return {
-//       message: "Hello world!",
-//     };
-//   },
-//   beforeCreate() {
-//     console.log("beforeCreate() function called!", this.message);
-//   },
-//   created() {
-//     console.log("created() function called!", this.message);
-//   },
-//   beforeMount() {
-//     //$el == value of element Vue is mounted to
-//     console.log("beforeMount() function called!", this.$el);
-//   },
-//   mounted() {
-//     console.log("mounted() function called!", this.$el);
-//   },
-//   beforeUpdate() {
-//     console.log("beforeUpdate() function called!");
-//   },
-//   updated() {
-//     console.log("updated() function called!");
-//   },
-//   beforeUnmount() {
-//     console.log("beforeUnmount() function called!");
-//   },
-//   unmounted() {
-//     console.log("unmounted() function called!");
-//   },
-// });
-//
-// vm.mount("#app");
-//
-// // setTimeout(() => {
-// //   vm.mount('#app')
-// // }, 3000)
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate() function called!", this.message);
+  },
+  created() {
+    console.log("created() function called!", this.message);
+  },
+  beforeMount() {
+    //$el == value of element Vue is mounted to
+    console.log("beforeMount() function called!", this.$el);
+  },
+  mounted() {
+    console.log("mounted() function called!", this.$el);
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate() function called!");
+  },
+  updated() {
+    console.log("updated() function called!");
+  },
+  beforeUnmount() {
+    console.log("beforeUnmount() function called!");
+  },
+  unmounted() {
+    console.log("unmounted() function called!");
+  },
+});
+
+vm.mount("#app");
+
+// setTimeout(() => {
+//   vm.mount('#app')
+// }, 3000)
