46fd6be0160bdc722562bdb621e3106dd6dcf22d
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-07-30 03:57:42 +0200

    Using Vue components

--- Section-4/Section-4-7/app.js
+++ Section-4/Section-4-7/app.js
@@ -1,26 +1,26 @@
 // understanding Reactivity using proxies and The Vue Compiler
-// let vm = Vue.createApp({
-//   //  template: ``
-// });
+let vm = Vue.createApp({
+  //  template: ``
+});
 
-// vm.component("hello", {
-//   template: `<h1>{{ message }}</h1>`,
-//   data() {
-//     return {
-//       message: "Hello World!",
-//     };
-//   },
-// });
+vm.component("hello", {
+  template: `<h1>{{ message }}</h1>`,
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+});
 
 vm.mount("#app");
 
-let vm2 = Vue.createApp({
-  data() {
-    return {
-      message: "Hello world!",
-    };
-  },
-  render() {
-    return Vue.h("h1", this.message);
-  },
-}).mount("#app");
+// let vm2 = Vue.createApp({
+//   data() {
+//     return {
+//       message: "Hello world!",
+//     };
+//   },
+//   render() {
+//     return Vue.h("h1", this.message);
+//   },
+// }).mount("#app");
