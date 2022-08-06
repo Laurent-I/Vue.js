8da4ae5d295bffcad6edcbcf081feb3635768b96
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-08-07 01:17:09 +0200

    Animating list using the transition-group component and the animate.css library

--- Section-7/src/App.vue
+++ Section-7/src/App.vue
@@ -23,22 +23,22 @@
     <h2 v-if="flag">Hey</h2>
   </transition>
 
-<!--  <button @click="addItem">Add</button>-->
+  <button @click="addItem">Add</button>
 
-<!--  <ul>-->
-<!--    <transition-group name="fade"-->
-<!--      enter-active-class="animate__animated animate__flipInX"-->
-<!--      leave-active-class="animate__animated animate__flipOutX"-->
-<!--    >-->
-<!--      <li-->
-<!--        v-for="(number, index) in numbers"-->
-<!--        :key="number"-->
-<!--        @click="removeItem(index)"-->
-<!--      >-->
-<!--        {{ number }}-->
-<!--      </li>-->
-<!--    </transition-group>-->
-<!--  </ul>-->
+  <ul>
+    <transition-group name="fade"
+      enter-active-class="animate__animated animate__flipInX"
+      leave-active-class="animate__animated animate__flipOutX"
+    >
+      <li
+        v-for="(number, index) in numbers"
+        :key="number"
+        @click="removeItem(index)"
+      >
+        {{ number }}
+      </li>
+    </transition-group>
+  </ul>
 </template>
 
 <script>
@@ -47,18 +47,18 @@ export default {
   data() {
     return {
       flag: true,
-      // numbers: [1, 2, 3, 4, 5],
+      numbers: [1, 2, 3, 4, 5],
     };
   },
   methods: {
-  //   addItem() {
-  //     const num = Math.floor(Math.random() * 100 + 1);
-  //     const index = Math.floor(Math.random() * this.numbers.length);
-  //     this.numbers.splice(index, 0, num);
-  //   },
-  //   removeItem(index) {
-  //     this.numbers.splice(index, 1);
-  //   },
+    addItem() {
+      const num = Math.floor(Math.random() * 100 + 1);
+      const index = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(index, 0, num);
+    },
+    removeItem(index) {
+      this.numbers.splice(index, 1);
+    },
     beforeEnter(el) {
       console.log("before-enter event fired", el);
     },
@@ -98,13 +98,13 @@ export default {
 </script>
 
 <style>
-/*.animate__flipOutX {*/
-/*  position: absolute;*/
-/*}*/
+.animate__flipOutX {
+  position: absolute;
+}
 
-/*.animate__animated {*/
-/*  animation-duration: 1.5s;*/
-/*}*/
+.animate__animated {
+  animation-duration: 1.5s;
+}
 
 li {
   font-size: 22px;
